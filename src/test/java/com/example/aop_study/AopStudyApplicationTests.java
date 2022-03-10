c14c9b755f8f15701183ebbc27d15511f438f73b
iqpizza6349 <iqpizza6349@gmail.com>
2022-03-10 19:50:39 +0900

    SetUp

---
+++ src/test/java/com/example/aop_study/AopStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aop_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
