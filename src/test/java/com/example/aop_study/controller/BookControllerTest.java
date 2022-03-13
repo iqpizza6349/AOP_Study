5eb11e2efafd7f3b91d7922696cad1715a47667d
iqpizza6349 <iqpizza6349@gmail.com>
2022-03-13 11:44:17 +0900

    도서 기부 테스트 케이스 추가

---
+++ src/test/java/com/example/aop_study/controller/BookControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.aop_study.controller;
+
+import com.example.aop_study.library.domain.entity.Patron;
+import com.example.aop_study.library.domain.repository.PatronRepository;
+import com.example.aop_study.library.dto.BookRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PatronRepository patronRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void before() {
+        Patron patron = Patron.builder()
+                .name("워크샵6349")
+                .build();
+        patronRepository.save(patron);
+    }
+
+    @Test
+    @Rollback()
+    public void 도서_기부_성공() throws Exception {
+        String title = "도서 제목";
+        String author = "작가";
+        String publisher = "워크샵 출판사";
+        String patron = "워크샵6349";
+        BookRequestDto bookRequestDto = new BookRequestDto(title, author, publisher);
+        bookRequestDto.setPatron(patron);
+
+        mockMvc.perform(
+                post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookRequestDto))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title", title).exists())
+                .andExpect(jsonPath("$.author", author).exists())
+                .andExpect(jsonPath("$.publisher", publisher).exists())
+                .andExpect(jsonPath("$.patron", patron).exists())
+                .andDo(print());
+    }
+
+}
